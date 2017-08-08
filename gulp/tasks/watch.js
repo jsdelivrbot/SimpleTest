4e698df4701d7bd8a324ddaa555b738031be5410
Bart Brusselmans <bart.brusselmans@bagaar.be>
2017-08-08 18:30:03 +0200

    Added gulp watch tasks and browsersync functionality

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,40 @@
+var gulp = require('gulp'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create();
+
+gulp.task('watch', () => {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "temp"
+    }
+  });
+
+  watch('./app/*.html', () => {
+    gulp.start('htmlRefresh');
+  });
+
+  watch('./app/styles/**/*.scss', () => {
+    gulp.start('cssInject');
+  });
+
+  watch('./app/scripts/**/*.js', () => {
+    gulp.start('scriptsRefresh');
+  });
+
+});
+
+gulp.task('htmlRefresh', ['copyHtml'], () => {
+  browserSync.reload();
+});
+
+gulp.task('cssInject', ['sass'], () => {
+  return gulp
+    .src('./temp/styles/app.css')
+		.pipe(browserSync.stream());
+});
+
+gulp.task('scriptsRefresh', ['scripts'], () => {
+  browserSync.reload();
+});
