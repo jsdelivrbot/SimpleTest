166c7f956c640ca35406ce7ca5203e3e1c1f0a8d
Bart Brusselmans <bart.brusselmans@bagaar.be>
2017-08-17 18:25:45 +0200

    Added history API fallback

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
+    historryApiFallback = require('connect-history-api-fallback'),
     browserSync = require('browser-sync').create();
 
 gulp.task('watch', () => {
@@ -7,7 +8,10 @@ gulp.task('watch', () => {
   browserSync.init({
     notify: false,
     server: {
-      baseDir: "temp"
+      baseDir: "temp",
+      middleware: [
+        historryApiFallback()
+      ]
     }
   });
 
